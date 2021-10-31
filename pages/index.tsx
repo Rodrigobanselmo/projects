afb87a22e872a8913a140dfc3295ad28d59a1c16
Rodrigo Anselmo <69397304+Rodrigobanselmo@users.noreply.github.com>
2021-10-31 11:31:39 -0300

    fix: add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,12 @@
 import React from 'react'
 
 import type { NextPage } from 'next'
-import OrgTreeComponent, { useTree } from 'react-drag-hierarchy-tree'
-
-const data = {
-  id: 1,
-  label: 'President',
-  children: [
-    {
-      id: 2,
-      label: 'Administrative',
-      children: [
-        {
-          id: 3,
-          label: 'Director',
-          children: [],
-        },
-      ],
-    },
-    {
-      id: 4,
-      label: 'Finances',
-      children: [
-        {
-          id: 5,
-          label: 'Seller',
-          children: [],
-        },
-      ],
-    },
-  ],
-}
+import { TextForm } from '../components/forms/TextForm'
 
 const Home: NextPage = () => {
-  const { treeRef } = useTree()
-
-  const onClick = () => {
-    treeRef.current?.onExpandNodes()
-  }
   return (
     <div>
-      <button onClick={onClick}>close/open</button>
-      <OrgTreeComponent data={data} ref={treeRef} horizontal />
+      <TextForm />
     </div>
   )
 }
